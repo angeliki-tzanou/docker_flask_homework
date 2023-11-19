f37892db53f7e80a5d62a44206533b6d8ddd7256
angeliki-tzanou <angeliki.tzanou@stonybrook.edu>
2023-11-19 15:14:20 +0000

    part 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask App!!!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
